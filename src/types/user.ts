00652102e8a6ea22ad74a97dd8af966ec5a98aa5
Karun <gyawalikarrun@gmail.com>
2025-11-16 21:18:59 +0545

    hooks store and types initialize

---
+++ src/types/user.ts
@@ -0,0 +1,15 @@
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone?: string;
+  website?: string;
+  company?: {
+    name: string;
+  };
+  address?: {
+    city?: string;
+    street?: string;
+  };
+}
