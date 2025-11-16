ba09478f2ad6f6dddd01cbb11efcc2cfc3a921a6
Karun <gyawalikarrun@gmail.com>
2025-11-16 23:24:11 +0545

    feat: setup main App layout with Sidebar and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/SideBar";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import DataTable from "./pages/DataTablePage";
+import HomePage from "./pages/HomPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="flex h-screen bg-background text-foreground">
+        <Sidebar />
+
+        <main className="flex-1 overflow-auto">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/data" element={<DataTable />} />
+            <Route path="/settings" element={<SettingsPage />} />
+          </Routes>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
